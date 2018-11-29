09ee7918cc283ba0e99168126f6e5419ac0c8c98
gen <genrocks88@gmail.com>
2018-11-29 01:19:10 -0500

    added initializing cudamalloc

--- research/pathIntegral.cu
+++ research/pathIntegral.cu
@@ -8,10 +8,22 @@ void add(int n, float *x, float *y)
   for (int i = 0; i < n; i++)
     y[i] = x[i] + y[i];
 }
+
 __global__
 void fill1D(int length, double matrix[], double num) {
     for (int i = 0; i < length; i++) {
         matrix[i] = num;
+    }
+}
+
+__global__
+void fill2D(double* delbmax, size_t pitch, int width, int height) {
+    for (int r = 0; r < height; ++r) {
+        double * row = (double*)((char*)delbmax + r * pitch);
+        for (int c = 0; c < width; ++c) {
+            double element = row[c];
+            // printf("%f", row[c]);
+        }
     }
 }
 
@@ -34,27 +46,80 @@ int main(void)
     fill1D<<<1, 256>>>(400, rc, 0.1);
     cudaDeviceSynchronize();
 
-    cudaMallocManaged(&);
+    // Host code
+    //width is columns and height is rows
+    int height = 400, width = 100;
+    size_t pitch;
+
+    double* delbmax;
+    cudaMallocPitch(&delbmax, &pitch, width * sizeof(double), height);
+    fill2D<<<100, 400000>>>(delbmax, pitch, width, height);
+
+    double* delamax;
+    cudaMallocPitch(&delamax, &pitch, width * sizeof(double), height);
+    fill2D<<<100, 400000>>>(delbmax, pitch, width, height);
+
+    double* delpmax;
+    cudaMallocPitch(&delpmax, &pitch, width * sizeof(double), height);
+    fill2D<<<100, 400000>>>(delbmax, pitch, width, height);
+
+    double* delcmax;
+    cudaMallocPitch(&delcmax, &pitch, width * sizeof(double), height);
+    fill2D<<<100, 400000>>>(delbmax, pitch, width, height);
+
+    double* ddelb;
+    cudaMallocPitch(&ddelb, &pitch, width * sizeof(double), height);
+    fill2D<<<100, 400000>>>(delbmax, pitch, width, height);
+
+    double* ddela;
+    cudaMallocPitch(&ddela, &pitch, width * sizeof(double), height);
+    fill2D<<<100, 400000>>>(delbmax, pitch, width, height);
+
+    double* ddelp;
+    cudaMallocPitch(&ddelp, &pitch, width * sizeof(double), height);
+    fill2D<<<100, 400000>>>(delbmax, pitch, width, height);
+
+    double* ddelc;
+    cudaMallocPitch(&ddelc, &pitch, width * sizeof(double), height);
+    fill2D<<<100, 400000>>>(delbmax, pitch, width, height);
+
+    double* del_bi;
+    cudaMallocPitch(&del_bi, &pitch, width * sizeof(double), height);
+    fill2D<<<100, 400000>>>(delbmax, pitch, width, height);
+
+    double* del_ai;
+    cudaMallocPitch(&del_ai, &pitch, width * sizeof(double), height);
+    fill2D<<<100, 400000>>>(delbmax, pitch, width, height);
+
+    double* del_pi;
+    cudaMallocPitch(&del_pi, &pitch, width * sizeof(double), height);
+    fill2D<<<100, 400000>>>(delbmax, pitch, width, height);
+
+    double* del_ci;
+    cudaMallocPitch(&del_ci, &pitch, width * sizeof(double), height);
+    fill2D<<<100, 400000>>>(delbmax, pitch, width, height);
 
 
-    double ** delbmax = (double **) malloc(del_c * sizeof(double *));
 
 
-    for (int j = 2; j <= 400; j++) {
-        n = 100; //Counter of trajectories with final x-position outside of specified band in relation to ship's landing position
-        m=1;    //Counter for number of cycles in while loop
 
-
-        while (n > 90 && m < 10) {
-            delbmax = rb[j -1] * abs(del_con)
-
-        }
-    }
+    // for (int j = 2; j <= 400; j++) {
+    //     n = 100; //Counter of trajectories with final x-position outside of specified band in relation to ship's landing position
+    //     m=1;    //Counter for number of cycles in while loop
+    //
+    //
+    //     while (n > 90 && m < 10) {
+    //         delbmax = rb[j -1] * abs(del_con)
+    //
+    //     }
+    // }
 
     cudaFree(rb);
     cudaFree(ra);
     cudaFree(rp);
     cudaFree(rc);
 
-  return 0;
+    cudaFree(delbmax);
+
+    return 0;
 }
